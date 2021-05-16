f89b1248849109e247a6cc9c3ccacc1fcb2eb74a
wilyanto <wilyanto24@gmail.com>
2021-05-17 01:46:44 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Buyers
+Route::resource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
+
+// Categories
+Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+
+// Products
+Route::resource('products', ProductController::class, ['only' => ['index', 'show']]);
+
+// Sellers
+Route::resource('sellers', SellerController::class, ['only' => ['index', 'show']]);
+
+// Transactions
+Route::resource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
+
+// Users
+Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
