461333d1616023a2b3f90ce412adc295d79be296
wilyanto <wilyanto24@gmail.com>
2021-05-22 00:15:09 +0700

    handling HttpException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
@@ -63,6 +64,8 @@ class Handler extends ExceptionHandler
             return $this->errorResponse("The specified method for the request is invalid", 405);
         } else if ($e instanceof NotFoundHttpException) {
             return $this->errorResponse("The specified URL cannot be found", 403);
+        } else if ($e instanceof HttpException) {
+            return $this->errorResponse($e->getMessage(), $e->getStatusCode());
         }
         return parent::render($request, $e);
     }
