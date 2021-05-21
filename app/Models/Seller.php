575de66c0cc9f87b6b7cbbc7dd52e66c7e29327a
wilyanto <wilyanto24@gmail.com>
2021-05-22 00:48:20 +0700

    implementing SellerScope

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use App\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Seller extends User
 {
     use HasFactory;
+
+    protected static function boot() {
+        parent::boot();
+        static::addGlobalScope(new SellerScope);
+    }
 
     public function products() {
         return $this->hasMany(Product::class);
