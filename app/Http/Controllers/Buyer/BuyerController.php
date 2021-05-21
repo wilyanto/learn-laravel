09c16b27f8d649e81fa53923d963b4540087b007
wilyanto <wilyanto24@gmail.com>
2021-05-21 22:56:16 +0700

    make ApiREsponser in Traits

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 
-class BuyerController extends Controller
+class BuyerController extends ApiController
 {
     public function index()
     {
