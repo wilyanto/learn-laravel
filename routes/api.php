587f2cf1c1dfefb38eb9002f9d6a890db4509ee6
wilyanto <wilyanto24@gmail.com>
2021-05-22 14:10:39 +0700

    implementing index method of SellerBuyerController

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransacitonController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerBuyerController;
 use App\Http\Controllers\Seller\SellerCategoryController;
 use App\Http\Controllers\Seller\SellerController;
 use App\Http\Controllers\Seller\SellerTransactionController;
@@ -58,6 +59,7 @@ Route::resource('products', ProductController::class, ['only' => ['index', 'show
 Route::resource('sellers', SellerController::class, ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', SellerTransactionController::class, ['only' => ['index']]);
 Route::resource('sellers.categories', SellerCategoryController::class, ['only' => ['index']]);
+Route::resource('sellers.buyers', SellerBuyerController::class, ['only' => ['index']]);
 
 // Transactions
 Route::resource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
