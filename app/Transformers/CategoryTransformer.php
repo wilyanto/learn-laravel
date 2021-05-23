cd0858d6a0a31310a163901f137adb2166816822
wilyanto <wilyanto24@gmail.com>
2021-05-23 15:39:36 +0700

    implementing HATEOAS for Category Instance

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -39,6 +39,28 @@ class CategoryTransformer extends TransformerAbstract
             'created_at' => (string) $category->created_at,
             'updated_at' => (string) $category->updated_at,
             'deleted_at' => isset($category->deleted_at) ? (string) $category->deleted_at : null,
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('categories.show', $category->id),
+                ],
+                [
+                    'rel' => 'category.buyers',
+                    'href' => route('categories.buyers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.products',
+                    'href' => route('categories.products.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.sellers',
+                    'href' => route('categories.sellers.index', $category->id),
+                ],
+                [
+                    'rel' => 'category.transactions',
+                    'href' => route('categories.transactions.index', $category->id),
+                ],
+            ],
         ];
     }
 
