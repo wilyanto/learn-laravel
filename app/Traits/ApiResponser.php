d520f034d96485e0e67cebd6eb005c1899d3bfde
wilyanto <wilyanto24@gmail.com>
2021-05-23 15:13:39 +0700

    implementing custom perPage in pagination

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -79,12 +80,23 @@ trait ApiResponser
 
     protected function paginate(Collection $collection)
     {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
         $page = LengthAwarePaginator::resolveCurrentPage();
+        
         $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 
         $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
-            'path' =>LengthAwarePaginator::resolveCurrentPath(),
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
         ]);
         $paginated->appends(request()->all());
         return $paginated;
